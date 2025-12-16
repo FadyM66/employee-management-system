45043f6677ea200de3ed6ca30162f43a029c3e04
FadyM66 <fadymohsen62@gmail.com>
2025-12-16 04:00:29 +0200

    add database connection instance using drizzle ORM

---
+++ backend/src/db/instance.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { dbCredentials } from './drizzle.config.ts';
+
+const db = drizzle({
+  connection: dbCredentials,
+});
+
+export default db;
