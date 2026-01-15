7f14f2245e0183a3d9229685a91f15271ba45077
FadyM66 <fadymohsen62@gmail.com>
2026-01-15 02:24:47 +0200

    disable the ssl connection in drizzle configs

--- backend/src/db/drizzle.config.ts
+++ backend/src/db/drizzle.config.ts
@@ -6,7 +6,7 @@ export const dbCredentials = {
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
-  ssl: process.env.DATABASE_SSL != 'false',
+  ssl: false,
 };
 
 export default defineConfig({
