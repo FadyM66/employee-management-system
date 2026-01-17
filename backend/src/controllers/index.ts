d5e143bbfdeba4f1179ee9464a02dd1eeb771c2a
FadyM66 <fadymohsen62@gmail.com>
2026-01-17 23:54:47 +0200

    add the role creation endpoint

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth.ts';
 import userRouter from './users.ts';
+import rolesRouter from './roles.ts';
 
 const router = Router();
 
 router.use(authRouter);
-router.use(userRouter);
+router.use('/user', userRouter);
+router.use('/role', rolesRouter);
 
 export default router;
