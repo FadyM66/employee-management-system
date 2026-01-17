e0f42a8eb665635a167ca963390b2e8caf312ca0
FadyM66 <fadymohsen62@gmail.com>
2026-01-17 03:38:20 +0200

    add the domain error, used error codes so far

---
+++ backend/src/models/DomainError.ts
@@ -0,0 +1,23 @@
+type Code =
+  | 'invalid-credentials'
+  | 'internal-error'
+  | 'validation-error'
+  | 'malformed-input'
+  | 'not-found'
+  | 'conflict-error';
+const internalErrorCodes: Array<Code> = ['internal-error'];
+
+export default class DomainError extends Error {
+  #isInternal: boolean;
+  constructor(
+    public code: Code,
+    public details: undefined | { [key: string]: unknown } = undefined,
+  ) {
+    super(code);
+    this.#isInternal = internalErrorCodes.includes(code);
+  }
+
+  get isInternal() {
+    return this.#isInternal;
+  }
+}
