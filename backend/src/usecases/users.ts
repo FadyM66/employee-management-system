98067d89498e0e2d6f20a5e707fe2595d7e65610
FadyM66 <fadymohsen62@gmail.com>
2026-01-17 23:57:26 +0200

    separate the user creation, add the role creation

---
+++ backend/src/usecases/users.ts
@@ -0,0 +1,45 @@
+import db from '../db/index.ts';
+import DomainError from '../models/DomainError.ts';
+import { hashPassword } from '../utils/auth.ts';
+
+interface CreateUserReturn {
+  id: string;
+  email: string;
+  role: string;
+}
+async function createUser(
+  email: string,
+  password: string,
+  role: string,
+): Promise<CreateUserReturn> {
+  const hashedPassword = await hashPassword(password);
+
+  let user: CreateUserReturn;
+  try {
+    user = await db.users.insert(email, hashedPassword, role);
+  } catch (error) {
+    if ('cause' in error && error.cause.code === '23505') {
+      throw new DomainError('conflict-error', {
+        message: 'resource already exists.',
+      });
+    } else {
+      throw new DomainError('internal-error', {
+        error,
+      });
+    }
+  }
+
+  if (!user) {
+    throw new DomainError('internal-error', {
+      message: 'user was not created successfully.',
+    });
+  }
+
+  return user;
+}
+
+const user = {
+  createUser,
+};
+
+export default user;
