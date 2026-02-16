9136dce9f65da39a2ea07a03b2c46988415e06fa
FadyM66 <fadymohsen62@gmail.com>
2026-02-16 04:00:05 +0200

    refactor the login flow to response with the user object and his access token, also to use HTTPOnly cookie for the refresh token

--- backend/src/middlewares/endpointWrapper.ts
+++ backend/src/middlewares/endpointWrapper.ts
@@ -1,26 +1,10 @@
-import {
-  Request as ExpressRequest,
-  Response as ExpressResponse,
-  NextFunction,
-} from 'express';
+import { Request as ExpressRequest, Response as ExpressResponse, NextFunction } from 'express';
 
 export function endpointWrapper(
-  routeHandler: (
-    request: ExpressRequest,
-    response: ExpressResponse,
-    next: NextFunction,
-  ) => Promise<Record<string, any> | void>,
+  routeHandler: (request: ExpressRequest, response: ExpressResponse, next: NextFunction) => Promise<Record<string, any> | void>,
 ) {
-  return async (
-    request: ExpressRequest,
-    response: ExpressResponse,
-    next: NextFunction,
-  ) => {
-    const result: Record<string, any> | void = await routeHandler(
-      request,
-      response,
-      next,
-    );
+  return async (request: ExpressRequest, response: ExpressResponse, next: NextFunction) => {
+    const result: Record<string, any> | void = await routeHandler(request, response, next);
 
     response.json(result);
   };
